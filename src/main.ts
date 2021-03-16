ed1f06e5329b6550e9348a3f090e7eef49c324d0
OscarPetr <oskar.petr@outlook>
2021-03-16 13:46:12 +0100

    new

--- src/main.ts
+++ src/main.ts
@@ -13,68 +13,75 @@ function generate(length: number): string {
 class Dababase {
     path: string;
     idUsage: boolean;
+    source: string;
 
     constructor(path: string, idUsage: boolean) {
         this.path = path;
         this.idUsage = idUsage;
-    };
+        this.source = `${process.cwd()}/${this.path}`;
+    }
 
+    // SETUP METHOD
     setup() {
-        var source = `${process.cwd()}/${this.path}`;
-        var parent = path.dirname(source);
-
-        fs.readdir(parent, 'utf-8', (err, files) => {
+        fs.readdir(path.dirname(this.source), 'utf-8', (err, files) => {
             if (err) throw err;
 
             if (files.includes(this.path)) {} else {
-                fs.writeFile(source, '[]', (err) => {
+                fs.writeFile(this.source, '[]', (err) => {
                     if (err) throw err;
                 });
             }
         });
-    };
+    }
 
+    // INSERT METHOD
     insert(entries: object) {
-        var source = `${process.cwd()}/${this.path}`;
-        fs.readFile(source, (err, data) => {
+        fs.readFile(this.source, (err, data) => {
             var obj = JSON.parse(data);
 
-            if (this.idUsage) {
-                entries["_id"] = generate(8);
-            }
+            if (this.idUsage) entries["_id"] = generate(8);
             obj.push(entries);
 
             obj = JSON.stringify(obj);
-
             fs.writeFile(this.path, obj, (err) => {
                 if (err) throw err;
             });
         });
-    };
+    }
 
+    // QUERY METHOD
     query(entry: object, callback: (err: Error, query: object)) {
-        var source = `${process.cwd()}/${this.path}`;
-        fs.readFile(source, (err, data) => {
+        fs.readFile(this.source, (err, data) => {
             var obj = JSON.parse(data);
             var keys = Object.keys(entry)[0];
             var values = Object.values(entry)[0];
             
             callback(err, Array.from(obj).find((object) => object.hasOwnProperty(keys) && object[keys] === values))
         });
-    };
+    }
 
+    // QUERYALL METHOD
     queryAll(entry: object, callback: (err: Error, query: object[])) {
-        var source = `${process.cwd()}/${this.path}`;
-        fs.readFile(source, (err, data) => {
+        fs.readFile(this.source, (err, data) => {
             var obj = JSON.parse(data);
             var keys = Object.keys(entry)[0];
             var values = Object.values(entry)[0];
             
             callback(err, Array.from(obj).filter((object) => object.hasOwnProperty(keys) && object[keys] === values))
         });
-    };
+    }
 
-    remove(entries: object) {}
+    // REMOVE METHOD
+    remove(entry: object) {
+        fs.readFile(this.source, (err, data) => {
+            var obj = JSON.parse(data);
+            var keys = Object.keys(entry)[0];
+            var values = Object.values(entry)[0];
+            
+            var index = Array.from(obj).findIndex((object) => object.hasOwnProperty(keys) && object[keys] === values);
+            delete obj[index];
+        });
+    }
 }
 
-module.exports = {Dababase};
+module.exports = Dababase;
